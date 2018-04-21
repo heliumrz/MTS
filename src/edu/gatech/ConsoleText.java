7692455e8ea4c9ceeeccd0514ce6c9d585ab88f8
Zhan, Rui <heliumrz@gatech.edu>
2018-04-20 22:57:13 -0400

    fixed format

--- src/edu/gatech/ConsoleText.java
+++ src/edu/gatech/ConsoleText.java
@@ -3,46 +3,40 @@ package edu.gatech;
 import java.io.*;
 import javax.swing.*;
 
-public class ConsoleText extends JTextArea
-{
-private static final long serialVersionUID = 1L;
-/**
-* 
-*/
-public ConsoleText() {
-LoopedStreams ls = null;
-try {
-ls = new LoopedStreams();
-} catch (IOException e) {
-// TODO Auto-generated catch block
-e.printStackTrace();
-}
-PrintStream ps = new PrintStream(ls.getOutputStream(),true);
-System.setOut(ps);
-System.setErr(ps);
-startConsoleReaderThread(ls.getInputStream());
-} 
+public class ConsoleText extends JTextArea {
+	private static final long serialVersionUID = 1L;
 
-private void startConsoleReaderThread(InputStream inStream) 
-{
-final BufferedReader br =new BufferedReader(new InputStreamReader(inStream));
-new Thread(new Runnable() {
-public void run() {
-StringBuffer sb = new StringBuffer();
-try {
-String s;
-while( (s=br.readLine()) != null) {
-sb.setLength(0);
-append(sb.append(s).append("\n").toString());
-setCaretPosition(getText().length());
-}
-}
-catch(Exception e) {
-e.printStackTrace();
-} 
+	public ConsoleText() {
+		LoopedStreams ls = null;
+		try {
+			ls = new LoopedStreams();
+		} catch (IOException e) {
 
+			e.printStackTrace();
+		}
+		PrintStream ps = new PrintStream(ls.getOutputStream(), true);
+		System.setOut(ps);
+		System.setErr(ps);
+		startConsoleReaderThread(ls.getInputStream());
+	}
 
-}
-}).start();
-}
+	private void startConsoleReaderThread(InputStream inStream) {
+		final BufferedReader br = new BufferedReader(new InputStreamReader(inStream));
+		new Thread(new Runnable() {
+			public void run() {
+				StringBuffer sb = new StringBuffer();
+				try {
+					String s;
+					while ((s = br.readLine()) != null) {
+						sb.setLength(0);
+						append(sb.append(s).append("\n").toString());
+						setCaretPosition(getText().length());
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		}).start();
+	}
 }
